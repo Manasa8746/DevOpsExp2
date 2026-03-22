ac073ec4da4fae184c877c6389cfdf96f621a708
Manasa8746 <manasamk2005@gmail.com>
2026-03-22 10:46:05 +0530

    first commit

---
+++ src/main/java/org/example/MainClass.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class MainClass {
+        public static void main(String[] args) {
+            System.out.println("Hello, this is a simple output from the main class!");
+        }
+    }
+
+
+
